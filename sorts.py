5ff9ae1a86c8692756d19cdd933c8f846661e83b
Neil Satra <github.k6p@gishpuppy.com>
2014-01-24 15:55:34 +0000

    Add mergesort and tests for both mergesort and merge functions

--- sorts.py
+++ sorts.py
@@ -13,8 +13,31 @@ def bubblesort(xs):
                 swapped = True
     return xs
 
+def mergesort(xs):
+    if(len(xs) < 2):
+        return xs
+    
+    middleindex = int(len(xs)/2)
+    firsthalf = xs[0:middleindex]
+    secondhalf = xs[middleindex:len(xs)]
+    return merge(mergesort(firsthalf),mergesort(secondhalf))
+
+def merge(first,second):
+    merged = []
+    while(len(first) > 0 and len(second) > 0):
+        if(first[0] <= second[0]):
+            merged.append(first.pop(0))
+        else:
+            merged.append(second.pop(0))
+    merged.extend(first)
+    merged.extend(second)
+    return merged
+
 def test():
     xs = [3,2,4,5,1]
     print(bubblesort(xs))
 
-test()
+#test()
+x1 = [1,3,4]
+x2 = [2,5]
+print(merge(x1,x2))
--- testsorts.py
+++ testsorts.py
@@ -1,33 +1,114 @@
 import unittest
 import sorts
 
-class Test_testsorts(unittest.TestCase):
+class Test_bubblesort(unittest.TestCase):
+    def setUp(self):
+        self.sortfunction = sorts.bubblesort
 
-    def test_bubblesortempty(self):
+    def test_empty(self):
         unsorted = []
         sorted = []
-        self.assertEqual(sorted, sorts.bubblesort(unsorted))
+        self.assertEqual(sorted, self.sortfunction(unsorted))
 
-    def test_bubblesortsimple(self):
+    def test_simple(self):
         unsorted = [3,2,4,5,1]
         sorted = [1,2,3,4,5]
-        self.assertEqual(sorted, sorts.bubblesort(unsorted))
+        self.assertEqual(sorted, self.sortfunction(unsorted))
 
-    def test_bubblesortsorted(self):
+    def test_sorted(self):
         unsorted = [1,2,3,4,5]
         sorted = [1,2,3,4,5]
-        self.assertEqual(sorted, sorts.bubblesort(unsorted))
+        self.assertEqual(sorted, self.sortfunction(unsorted))
         
-    def test_bubblesortreversed(self):
+    def test_reversed(self):
         unsorted = [5,4,3,2,1]
         sorted = [1,2,3,4,5]
-        self.assertEqual(sorted, sorts.bubblesort(unsorted))
+        self.assertEqual(sorted, self.sortfunction(unsorted))
 
-    def test_bubblesortduplicates(self):
+    def test_duplicates(self):
         unsorted = [3,2,1,2,4]
         sorted = [1,2,2,3,4]
-        self.assertEqual(sorted, sorts.bubblesort(unsorted))
+        self.assertEqual(sorted, self.sortfunction(unsorted))
 
+    def test_letters(self):
+        unsorted = ['c','b','d','e','a']
+        sorted = ['a','b','c','d','e']
+        self.assertEqual(sorted, self.sortfunction(unsorted))
+
+    def test_negatives(self):
+        unsorted = [3, -1, 0, -4, 2]
+        sorted = [-4, -1, 0, 2, 3]
+        self.assertEqual(sorted, self.sortfunction(unsorted))
+
+class Test_mergesort(unittest.TestCase):
+    def setUp(self):
+        self.sortfunction = sorts.mergesort
+
+    def test_empty(self):
+        unsorted = []
+        sorted = []
+        self.assertEqual(sorted, self.sortfunction(unsorted))
+
+    def test_simple(self):
+        unsorted = [3,2,4,5,1]
+        sorted = [1,2,3,4,5]
+        self.assertEqual(sorted, self.sortfunction(unsorted))
+
+    def test_sorted(self):
+        unsorted = [1,2,3,4,5]
+        sorted = [1,2,3,4,5]
+        self.assertEqual(sorted, self.sortfunction(unsorted))
+        
+    def test_reversed(self):
+        unsorted = [5,4,3,2,1]
+        sorted = [1,2,3,4,5]
+        self.assertEqual(sorted, self.sortfunction(unsorted))
+
+    def test_duplicates(self):
+        unsorted = [3,2,1,2,4]
+        sorted = [1,2,2,3,4]
+        self.assertEqual(sorted, self.sortfunction(unsorted))
+
+    def test_letters(self):
+        unsorted = ['c','b','d','e','a']
+        sorted = ['a','b','c','d','e']
+        self.assertEqual(sorted, self.sortfunction(unsorted))
+
+    def test_negatives(self):
+        unsorted = [3, -1, 0, -4, 2]
+        sorted = [-4, -1, 0, 2, 3]
+        self.assertEqual(sorted, self.sortfunction(unsorted))
+
+class Test_merge(unittest.TestCase):
+    def test_mergeempties(self):
+        first = []
+        second = []
+        merged = []
+        self.assertEqual(merged, sorts.merge(first,second))
+
+    def test_mergeoneemptylist(self):
+        first = []
+        second = [1,2,3]
+        merged = [1,2,3]
+        self.assertEqual(merged, sorts.merge(first, second))
+
+    def test_mergeotheremptylist(self):
+        first = [1,2,3]
+        second = []
+        merged = [1,2,3]
+        self.assertEqual(merged, sorts.merge(first, second))
+
+    def test_merge_balanced_lists(self):
+        first = [1,3,4]
+        second = [2,5,6]
+        merged = [1,2,3,4,5,6]
+        self.assertEqual(merged, sorts.merge(first,second))
+
+    def test_merge_unbalanced_lists(self):
+        first = [1,3,4,6]
+        second = [2,5]
+        merged = [1,2,3,4,5,6]
+        self.assertEqual(merged, sorts.merge(first,second))
 
 
 if __name__ == '__main__':
